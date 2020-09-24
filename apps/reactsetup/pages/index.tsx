292ab165fe0208047e29fbf86792f366391f77e0
Matheus Neves <matheusneves1989@gmail.com>
2020-09-24 10:37:55 -0300

    chore: Added typescript

---
+++ apps/reactsetup/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <main >
+        <h1>
+          Hello World
+        </h1>
+      </main>
+    </>
+  )
+}
