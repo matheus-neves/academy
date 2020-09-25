5adab9470839447b151bcd33d37352a31b6ab551
Matheus Neves <matheusneves1989@gmail.com>
2020-09-25 19:26:20 -0300

    feat: Added home page

--- apps/reactsetup/src/pages/index.tsx
+++ apps/reactsetup/src/pages/index.tsx
@@ -1,16 +1,15 @@
+import React from 'react'
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <>
       <Head>
         <title>Homepage</title>
       </Head>
 
-      <main >
-        <h1>
-          Hello World
-        </h1>
+      <main>
+        <h1>Hello World</h1>
       </main>
     </>
   )
