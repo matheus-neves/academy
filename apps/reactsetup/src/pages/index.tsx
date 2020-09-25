5d6cffb6621fcee76be1f56ebd4d3b768d30e506
Matheus Neves <matheusneves1989@gmail.com>
2020-09-25 20:09:39 -0300

    feat: Added next-images

--- apps/reactsetup/src/pages/index.tsx
+++ apps/reactsetup/src/pages/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import Head from 'next/head'
 
+import logo from '../assets/logo.svg'
+import { Container } from '../styles/pages/Home'
+
 export default function Home(): JSX.Element {
   return (
-    <>
+    <Container>
       <Head>
         <title>Homepage</title>
       </Head>
 
-      <main>
-        <h1>Hello World</h1>
-      </main>
-    </>
+      <img src={logo} alt="Logo NextJS" />
+      <h1>Setup NextJS</h1>
+      <p>A ReactJS + Next.js structure</p>
+    </Container>
   )
 }
